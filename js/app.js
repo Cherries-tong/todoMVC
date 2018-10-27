5c87343f0abd471bb9f39c7fc134cb3ca8fa90c9
Cherries-tong <17601010432@163.com>
2018-10-27 09:06:57 +0800

    todomvc未完成版

--- js/app.js
+++ js/app.js
@@ -1,6 +1,80 @@
-(function (window) {
-	'use strict';
+(function (window,Vue) {
 
-	// Your starting point. Enjoy the ride!
+	var arr =[
+		{
+			id:1,
+			content:'abc',
+			isFinish:true
+		},
+		{
+			id:2,
+			content:'abc',
+			isFinish:false
+		},
+		{
+			id:3,
+			content:'abc',
+			isFinish:true
+		}
+	]
 
-})(window);
+	new Vue({
+		el:'#app',
+		data:{
+			dataList:JSON.parse(window.localStorage.getItem('dataList')) || [],
+			newTodo:''
+		},
+		directives:{
+			//获取光标
+			focus:{
+				inserted(el){
+					el.focus();
+				}
+			}
+		},
+		methods:{
+			//添加
+			addTodo(){
+				if(!this.newTodo.trim()) return;
+				this.dataList.unshift({
+					id:this.dataList.length?this.dataList.sort((a,b)=>(a.id-b.id))[this.dataList.length - 1]['id']+1 : 1,
+					content:this.newTodo.trim(),
+					isFinish:false
+				})
+				this.newTodo = ''
+			},
+			//删除单个
+			delTodo(i){
+				this.dataList.splice(i,1)
+			},
+			//删除所有
+			delAll(){
+				this.dataList = this.dataList.filter(item=>!item.isFinish)
+			}
+		},
+		//isFinish
+		watch:{
+			dataList:{
+				handler(newArr){
+					window.localStorage.setItem('dataList',JSON.stringify(newArr))
+				},
+				deep:true
+			}
+		},
+		computed:{
+			todoCount(){
+				return this.dataList.filter(item =>!item.isFinish).length;
+			},
+			//全选反选
+			toggleAll:{
+				get(){
+					return this.dataList.every(item=>item.isFinish)
+				},
+				set(val){
+					this.dataList.forEach(item=>item.isFinish=val)
+				}
+			}
+		}
+	})
+
+})(window,Vue);
